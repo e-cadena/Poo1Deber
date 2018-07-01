f7b13f5f07fc537108735703e2acea17bab3528b
e-cadena <36049644+e-cadena@users.noreply.github.com>
2018-07-01 13:54:32 -0500

    Add files via upload

---
+++ clases.cs
@@ -0,0 +1,29 @@
+
+public class futol
+{
+    public class Arco
+    {
+        string material;
+    }
+
+    public class Cancha
+    {
+        string largo;
+    }
+
+    public class Entrenador
+    {
+        string logros;
+    }
+
+    public class Jugador
+    {
+        string nacionalidad; 
+        
+    }
+
+    public class Pelota
+    {
+        string marca;
+    }
+}
